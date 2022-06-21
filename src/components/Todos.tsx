bcfcfbb14b8819cd10ce35f62e9b49a9beedff4b
vodjanoj <vladimirs.vodjanojs@gmail.com>
2022-06-21 12:55:15 +0300

    Adding Styling

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import TodoItem from './TodoItem'
 import Todo from "../models/todo"
+import classes from './Todos.module.css'
 
 // We can use such a generic type for props or to be precise, a functional component out of the box
 // can be turned, can be converted into a generic function. Which then simply means that in the end
@@ -18,7 +19,7 @@ import Todo from "../models/todo"
 // Todo, we can set class as a type
 const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
-    <ul>
+    <ul className={classes.todos}>
       {props.items.map((item) => (
         <TodoItem text={item.text} key={item.id}/>
       ))}
